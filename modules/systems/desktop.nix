9e6ab4965ee2e056b1828fc977cebb4bc4231e34
Apollo-sudo767 <blured767@gmail.com>
2025-03-11 14:53:33 -0500

    Added stylix

--- modules/systems/desktop.nix
+++ modules/systems/desktop.nix
@@ -97,7 +97,45 @@
   # Power profiles and policy kit
   # services.power-profiles-daemon.enable = true; # Battery Daemon replaced by TLP
   security.polkit.enable = true;
-   
+  
+   # Stylix
+  stylix = {
+    enable = true;
+    image = pkgs.fetchurl {
+      url = "https://live.staticflickr.com/65535/52211883799_9642668157_o_d.png";
+      sha256 = "5bdc52f583da6dc14c26ac3f00d1e5ff45ae7a1da3820d5abf4076b512e18e76";
+    };
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "Dejavu Sans";
+
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "Dejavu Serif";
+      };
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 12;
+      };
+    };
+    opacity = {
+      applications = 1.0;
+      terminal = 0.8;
+      desktop = 1.0;
+      popups = 1.0;
+    };
+    polarity = "dark";
+  };
+
   # Services
   services.dbus.packages = [pkgs.gcr];
   services.geoclue2.enable = true;
